e15d5242c6b0d9f42cb2493d77b1a680fe3a9618
student <student@PC_105_1->
2019-02-09 19:17:58 +0200

    release1

--- WpfAppDemo1/MainWindow.xaml.cs
+++ WpfAppDemo1/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,23 @@ namespace WpfAppDemo1
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<string> collection =
+            new ObservableCollection<string>() { "str1", "str2", "str3" };
         public MainWindow()
         {
             InitializeComponent();
+            stringsListBox.ItemsSource = collection;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string inputString = inputTextBox.Text;
+            targetTextBlock.Text = inputString;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true) {
+                MessageBox.Show(openFileDialog.FileName);
+            }
         }
     }
 }
